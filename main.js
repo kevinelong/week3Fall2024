4c54af2265d85cafaed8bb82dd0b39470011a4f2
kevinelong <kevinelong@gmail.com>
2024-10-07 09:15:24 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+a = 6           // declaration, statement, assigning the value 3 to the identifier "a"
+b = 7           // variable b is 5
+c = a + b       // expression a+b adding a and b, then assigning to variable c
+console.log(c)  // passing c into the log method/function of the console object.
+
+//F5 fn+5 RUN/PLAY
+
+let firstName = "Kevin";
+let greeting = "Hello ";
+
+// Concatenate operator - "Chain together" like a train.
+let message = greeting + firstName; 
+
+console.log(message);
