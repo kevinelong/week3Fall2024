e9906a3b73c38ae72e53d2ed8b913c33811e489b
kevinelong <kevinelong@gmail.com>
2024-10-09 09:06:11 -0700

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+
+// COMPUTER VALUES ARE 0 and 1, AKA OFF and ON
+// LOGIC TRUE OR FALSE, 0 is usually false and non-zero is usually true
+
+// Bool - Boolean (True/False) Data Type - Reasone/Philosophy into math?
+
+//INPUTS
+hasCar = true
+hasLicense = true
+canReachPedals = true
+
+//CALCULATION
+console.log("START")
+
+//DECISION
+if(hasLicense && hasCar && canReachPedals){ //acts as a gate
+    //CONDITIONAL CODE MIGHT NOT BE RUN/EXECUTED
+    console.log("Zoom");
+    console.log("Zoom");
+}else{
+    console.log("Cry and suffer.")
+}
+
+//BACK IN MAIN LIN OF CODE
+console.log("FINISH")
