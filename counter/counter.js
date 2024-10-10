59df2d5d93f423b9e3fcdcdf36d9c48804d71b91
kevinelong <kevinelong@gmail.com>
2024-10-10 12:17:36 -0700

    counter web

---
+++ counter/counter.js
@@ -0,0 +1,52 @@
+// //counter.js
+let componentCounter = 0;
+function moveUp(){
+    componentCounter++;
+}
+function moveDown(){
+    componentCounter--;
+}
+function getCounter(){
+    return componentCounter;
+}
+// moveUp();
+// moveUp();
+// moveUp();
+// moveUp();
+// moveUp(); //5
+// moveDown();
+// moveDown();
+// moveDown(); //2
+// console.log(getCounter()); //2
+
+// value = 0
+
+// value++; // ++ increment by one
+// value++; // ++ increment by one
+
+// console.log(value) //expecting 2
+
+// beers = 99
+// beers--;
+// beers--;
+// beers--;
+// beers--;
+// console.log(beers) // 99 - 4 = 95
+
+// counterValue = 0
+// incrementAmount = 5
+
+// counterValue += incrementAmount
+// counterValue += incrementAmount
+// counterValue += incrementAmount
+// counterValue += 5
+// counterValue += 5
+
+// console.log(counterValue) //expect 15
+
+// current = 0
+// console.log(current++)
+
+// pre = 0
+// console.log(++pre)
+
