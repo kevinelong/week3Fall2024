6bbafda7599c16287038de094bcd9c9e823cb9ca
kevinelong <kevinelong@gmail.com>
2024-10-11 09:12:33 -0700

    latest

--- calc/main.js
+++ calc/main.js
@@ -1,33 +1,39 @@
 //calc
 
-function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
 
-function subtract(a, b) {
-    return a - b;
-}
+// function subtract(a, b) {
+//     return a - b;
+// }
 
-function multiply(a, b) {
-    return a * b;
-}
+// function multiply(a, b) {
+//     return a * b;
+// }
 
-function divide(a, b) {
-    return a / b;
-}
+// function divide(a, b) {
+//     return a / b;
+// }
 
 function calc(a, b, operation) {
-    let result = 0;
-    if ("add" == operation) {
-        result = add(a, b);
-    } else if ("subtract" == operation) {
-        result = subtract(a, b);
-    } else if ("divide" == operation) {
-        result = divide(a, b);
-    } else if ("multiply" == operation) {
-        result = multiply(a, b);
+    switch (operation) {
+        case "add": return a + b; 
+        case "subtract": return a - b; 
+        case "multiply": return a * b; 
+        case "divide": return a / b;
     }
-    return result;
+    // let result = 0;
+    // if ("add" == operation) {
+    //     result = add(a, b);
+    // } else if ("subtract" == operation) {
+    //     result = subtract(a, b);
+    // } else if ("divide" == operation) {
+    //     result = divide(a, b);
+    // } else if ("multiply" == operation) {
+    //     result = multiply(a, b);
+    // }
+    // return result;
 }
 
 console.log(calc(6, 7, "add"));
